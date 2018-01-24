d74f6b4f669cd3fb6490819427650965f2a07238
Jose Hidalgo <c0d5x@protonmail.com>
2018-01-24 11:33:41 -0600

    initial commit

---
+++ BusyApp.java
@@ -0,0 +1,11 @@
+
+public final class BusyApp {
+  public static void main(String []args) throws Exception {
+    final java.util.Random generator = new java.util.Random();
+    while (true) {
+      generator.ints(1000000, 0, 100).sorted();
+      Thread.sleep(5000L);
+    }
+  }
+}
+
